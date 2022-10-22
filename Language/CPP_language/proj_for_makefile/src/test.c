072e0c5fab3dd1b8b2fd6baae5ee69e2750cae21
qiluocao <zhourongtu@qq.com>
2022-10-22 11:41:03 +0800

    添加了基本的makefile的处理

---
+++ Language/CPP_language/proj_for_makefile/src/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc,char* argv[])
+{
+    int count = 0;
+    printf("The command line has %d arguments :\n", argc);
+    for (count = 0; count < argc; ++count) {
+        printf("%d: %s\n",count,argv[count]);
+    }
+    return 0;
+}
